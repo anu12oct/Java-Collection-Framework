bb6d3e2ef36f308a5146007be6dc3461e7179f5c
Anupam Sharma <sharma12oct@gmail.com>
2022-05-20 08:20:07 +0530

    Add files via upload

---
+++ Basic Problems/Method.java
@@ -0,0 +1,20 @@
+import java.math.*;
+import java.io.*;
+import java.util.*;
+
+public class Method {
+    public static void main(String[] args) throws IOException {
+        int x = 5, y = 10;
+
+        System.out.println(getMax(x, y));
+    }
+
+    public static int getMax(int x, int y) {
+        if (x > y)
+            return x;
+
+        else
+            return y;
+    }
+
+}
